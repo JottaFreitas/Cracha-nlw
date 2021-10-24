c93c2d0df3ecef29e7652fdab593cc57f616db06
Josué Freitas <92505284+JottaFreitas@users.noreply.github.com>
2021-10-24 02:21:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+//function ShowMeSomething (){} //aqui abrimos uma função, que tem alguma ação a ser feita posteriormente. Tem que conter (){} nessa ordem.//
+
+const LinksSocialMedia = {
+  github: 'jakeliny',
+  youtube: 'jakelinygracielly',
+  facebook: 'mayk.brito',
+  instagram: 'jakeliny.gracielly'
+  //ao definir uma var dentro de um objeto, não se usa = mas sim : //
+}
+
+function changeSocialMediaLinks() {
+  //document.getElementById('username')
+  //userName.textContent = 'Olívio' //document=buscar no DOM, getElement=pegar um elemento ById=pegar pelo ID ('nome do id')//
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGithubProfileInfos()
+
+//aqui chamamos a função a ser executada.//
